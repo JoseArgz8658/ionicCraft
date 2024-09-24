5319d64d761b9e1e914a4aff81f032eaaecad037
JoseArgz8658 <jose.miguel11.bkn@gmail.com>
2024-09-24 20:59:47 -0300

    Update del MENU y Modo oscuro
    El menú ahora funciona correctamente y sobre el modo oscuro quite la opción y ahora no tendrá de alternar entre modo claro y modo oscuro, ahora siempre será modo oscuro ya que visualmente es mas agradable a la vista.

--- src/app/configuraciones/configuraciones.page.ts
+++ src/app/configuraciones/configuraciones.page.ts
@@ -6,32 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./configuraciones.page.scss'],
 })
 export class ConfiguracionesPage implements OnInit {
-  paletteToggle = false;
 
-  constructor() {
-    this.initDarkMode();
-  }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  initDarkMode() {
-    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
-    this.initializeDarkPalette(prefersDark.matches);
-    prefersDark.addEventListener('change', (mediaQuery) => this.initializeDarkPalette(mediaQuery.matches));
-  }
-
-  initializeDarkPalette(isDark: any) {
-    this.paletteToggle = isDark;
-    this.toggleDarkPalette(isDark);
-  }
-
-  toggleChange(ev: any) {
-    this.toggleDarkPalette(ev.detail.checked);
-  }
-
-  toggleDarkPalette(shouldAdd: boolean) {
-    document.documentElement.classList.toggle('ion-palette-dark', shouldAdd);
   }
 
 }
