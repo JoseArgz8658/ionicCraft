7a13c9e43b8290699ac78a45ebca167d77ddec29
JoseArgz8658 <jose.miguel11.bkn@gmail.com>
2024-10-21 17:17:24 -0300

    update

--- src/app/CrudUsuarios/create-usuarios/create-usuarios.page.ts
+++ src/app/CrudUsuarios/create-usuarios/create-usuarios.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { BdService } from 'src/app/services/bd.service';
+import { Keyboard } from '@capacitor/keyboard';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-create-usuarios',
@@ -7,18 +9,32 @@ import { BdService } from 'src/app/services/bd.service';
   styleUrls: ['./create-usuarios.page.scss'],
 })
 export class CreateUsuariosPage implements OnInit {
-    usuario_tipo: string = "";
-    usuario_apodo: string = "";
-    usuario_gmail: string = "";
-    usuario_password: string = "";
+  @ViewChild('content', { static: false }) content: IonContent;
+
+  usuario_tipo: string = "";
+  usuario_apodo: string = "";
+  usuario_gmail: string = "";
+  usuario_password: string = "";
 
   constructor(private bd: BdService) { }
 
   ngOnInit() {
+    this.initializeKeyboardListeners();
   }
 
-  agregar(){
+  initializeKeyboardListeners() {
+    Keyboard.addListener('keyboardWillShow', () => {
+      this.content.scrollToBottom(300);
+    });
+
+    Keyboard.addListener('keyboardWillHide', () => {
+      this.content.scrollToTop(300);
+    });
+  }
+
+  agregar() {
     this.bd.agregarUsuarios(this.usuario_tipo, this.usuario_apodo, this.usuario_gmail, this.usuario_password);
+    Keyboard.hide();
   }
 
 }
